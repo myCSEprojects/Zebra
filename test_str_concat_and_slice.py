0241716344e5da4205426a003f184daae3ee51d3
Voorugonda Rajesh <76484557+RajeshVoorugonda@users.noreply.github.com>
2023-01-30 13:47:12 +0530

    Create test_str_concat_and_slice.py

---
+++ test_str_concat_and_slice.py
@@ -0,0 +1,13 @@
+def test():
+
+    print("Strings test cases:")
+    a = (str_concat(Str("abc"),Str("xyz")))
+    b = str_concat(Str("ppp"),a)
+    if (evaluate(str_concat(a,b)).value == "abcxyzpppabcxyz"):
+        print("Test 1/3 Passed")
+    a= evaluate(a)
+    if ( evaluate(Slice(Str("abc"),1,3)).value == "bc") : 
+        print("Test 2/3 Passed")
+    if (evaluate(BinOp('*',Str('abc'),Int(3)))).value == 'abcabcabc':
+        print("Test 3/3 Passed")
+        

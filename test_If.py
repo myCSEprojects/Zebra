164d08a2917d88cc632496d41c44f072c33adacc
Voorugonda Rajesh <76484557+RajeshVoorugonda@users.noreply.github.com>
2023-01-30 13:45:46 +0530

    Update test_If.py

--- test_If.py
+++ test_If.py
@@ -2,7 +2,7 @@ from sim import *
 
 def test():
     x = Variable("x")
-    condition = Let(x, Int(20), BinOp("<", x, Int(10)))
+    condition = Let(x, Int(20), BinOp("<", x, Int(20)))
     if (evaluate(If(condition, Int(10), Int(20))) != Int(10)):
         print("Basic evaluation of If failed")
         exit()

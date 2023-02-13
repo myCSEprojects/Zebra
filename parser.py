8c8c9adad1902de3d44bae16d9ea3878707e8ae4
Siva Sai Bommisetty <80308830+sivasai-133@users.noreply.github.com>
2023-02-13 23:37:28 +0530

    cleaning parser.py

--- parser.py
+++ parser.py
@@ -60,7 +60,6 @@ class Parser:
         self.lexer.match(Operator(';'))
         return t
     def parse_atom(self):
-        print(self.lexer.peek_token())
         match self.lexer.peek_token():
             case Identifier(name):
                 self.lexer.advance()
@@ -75,9 +74,7 @@ class Parser:
                 self.lexer.advance()
                 return Str(value)
             case Flt(value):
-                print(f"float {value}")
                 self.lexer.advance()
-                print(Float(value))
                 return Float(value)
             case Operator('('):
                 self.lexer.advance()

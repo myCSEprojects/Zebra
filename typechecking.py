975c3a8e2c94a20e57680147932f37008c6cc50f
Venkata Sriman <venkata.sriman@iitgn.ac.in>
2023-04-09 12:26:26 +0530

    Add classes without inheritence, perform type checking, add instantiation, getters, and setters

--- typechecking.py
+++ typechecking.py
@@ -74,18 +74,31 @@ def typecheck(program: AST, scopes = None):
     match program:
         case Float(): 
             return Float
+        
         case Int(): 
             return Int
+        
         case Bool(): 
             return Bool
+        
         case Variable(lineNumber, name, id) as v:
             return scopes.getVariableType(v)
+        
         case Str():
             return Str
+        
+        case This(lineNumber, id):
+            return scopes.getVariableType(Variable(lineNumber, "this", id))
+
         case nil():
             return nil
+        
+        case InstanceObject() as obj:
+            return instanceType(obj.zClass)
+        
         case zList(dtype, lst):
             return zList
+        
         case BinOp(lineNumber, operator, firstOperand, secondOperand):
             # Getting the operand types
             firstOperandType = typecheck(firstOperand, scopes)
@@ -93,11 +106,15 @@ def typecheck(program: AST, scopes = None):
             
             # Checking if the operator is defined
             if (operator not in BINARY_OPERATORS):
-                typeCheckError(f"Binary Operator {operator} not reconized", lineNumber)
+                typeCheckError(f"Binary Operator {operator} not reconized", lineNumber)              
 
-            # General BinOp type error string
-            errorString = f"Operator {operator} not defined between data types {firstOperandType.__name__} and {secondOperandType.__name__}"
+            # Raising type error in case left or the right side donot evaluate to a type
+            if (operator != '=' and (((not isinstance(firstOperandType, type)) and isinstance(firstOperandType, InstanceObject)) or ((not isinstance(secondOperandType, type)) and isinstance(secondOperandType, InstanceObject)))):
+                typeCheckError(f"Operator {operator} not defined between {firstOperandType} and {secondOperandType}", lineNumber)
 
+            if (operator != '='):
+                # General BinOp type error string
+                errorString = f"Operator {operator} not defined between data types {firstOperandType} and {secondOperandType}"
             match operator:
                 case  "+":
                     if (issubclass(firstOperandType, Str) and not checkTypeTwo(firstOperandType, secondOperandType, Str, Str)):
@@ -173,9 +190,12 @@ def typecheck(program: AST, scopes = None):
                 
                 case "=":                    
                     # Prevent assignment for lists (for now)
-                    if (issubclass(secondOperandType, zList)):
+                    if ((isinstance(secondOperandType, type)) and issubclass(secondOperandType, zList)):
                         typeCheckError(f"Cannot assign a list to a variable.", lineNumber)
-
+                    elif (isinstance(secondOperandType, instanceType)):
+                        scopes.updateVariable(firstOperand, InstanceObject(secondOperandType.name, {}))
+                        return secondOperandType
+                    
                     scopes.updateVariable(firstOperand, createDummyObject(secondOperandType))
                     return secondOperandType
                 
@@ -193,7 +213,10 @@ def typecheck(program: AST, scopes = None):
             
             if (operator not in UNARY_OPERATORS):
                 typeCheckError(f"Unary Operator {operator} not reconized", lineNumber)
-            
+            if (not isinstance(opearandType, type)):
+                if (isinstance(opearandType, InstanceObject)):
+                    typeCheckError(f"Operator {operator} is not defined for {opearandType}", lineNumber)
+
             match operator:
                 case "-":
                     if not issubclass(opearandType, Number):
@@ -207,7 +230,7 @@ def typecheck(program: AST, scopes = None):
                         typeCheckError(f"Operator {operator} is not defined for data type {opearandType.__name__}", lineNumber)
                     return Bool
 
-        case Declare(lineNumber, var, value, dtype, isConst):
+        case Declare(lineNumber, var, value, dtype, isConst) as dec:
             if (not isinstance(var, Variable)):
                 raise Exception("RHS of declaration must be of type \'Variable\'")
 
@@ -218,56 +241,84 @@ def typecheck(program: AST, scopes = None):
                 scopes.declareVariable(var, tempval, dtype, isConst)
             else:
                 # Evaluating the expression before declaration
-                
                 value = typecheck(value, scopes)
-                
-                # Declaring
-                scopes.declareVariable(var, createDummyObject(value), dtype, isConst)
+                if (not isinstance(dtype, type) and isinstance(dtype, instanceType)):
+                    if value != nil:
+                        if ((isinstance(value, type)) or not isinstance(value, instanceType)):
+                            typeCheckError(f"Cannot initialize {dtype.name} with {value}.", lineNumber)
+                        dec.dtype = instanceType(scopes.getVariable(dtype.name))
+                        scopes.declareVariable(var, InstanceObject(value.name, {}), instanceType(scopes.getVariable(dtype.name)), isConst)
+                    else:
+                        dec.dtype = instanceType(scopes.getVariable(dtype.name))
+                        scopes.declareVariable(var, nil(), instanceType(scopes.getVariable(dtype.name)), isConst)
+                else:
+                    scopes.declareVariable(var, createDummyObject(value), dtype, isConst)
             return dtype
 
         case Slice(lineNumber, value_, first, second):       #checking value_ is string 
             tc = typecheck(value_, scopes)
             tf = typecheck(first, scopes)
             ts = typecheck(second, scopes)
-            if (not(issubclass(tc, Str | zList))):
+            if ((not isinstance(tc, type)) or (not(issubclass(tc, Str | zList)))):
                 typeCheckError(f"Slice operation not defined for {type(tc).__name__}", lineNumber)
-            if not (issubclass(tf, Int) or issubclass(ts, Int)):
+            if (not isinstance(tf, type) or not isinstance(ts, type)) or (not (issubclass(tf, Int) or issubclass(ts, Int))):
                 typeCheckError(f"Slice indices must be of {Int} type", lineNumber)
-            if(issubclass(ts,nil) and issubclass(tc,zList)):
+            if (issubclass(ts,nil) and issubclass(tc,zList)):
                 element_type = scopes.getVariable(value_).dtype
                 return element_type
             return tc
 
         case While(lineNumber, condition, block) :            #checking while condition data type
             tc = typecheck(condition, scopes)
-            if (not(issubclass(tc, AST))):
-                raise Exception("Arguments passed to While block must be of 'AST' type")
             return typecheck(block, scopes)
         
         case If (lineNumber, condition, ifBlock, elseBlock): #checking if condition data type
             tc = typecheck(condition, scopes)
-            if (not (issubclass(tc, AST))):
-                raise Exception("Arguments passed to If block must be of 'AST' type")
-            return typecheck(ifBlock, scopes) | typecheck(elseBlock, scopes)
+            tif = typecheck(ifBlock, scopes)
+            telse = typecheck(elseBlock, scopes)
+            return tif + telse
+
 
         case For(lineNumber, initial,condition,block):
             scopes.beginScope()
             
             tc1 = typecheck(initial, scopes)
-            if(not(issubclass(tc1,AST))):
-                raise Exception("Arguments passes to For initial must be of 'AST' type")
             tc = typecheck(condition, scopes)
-            if(not( issubclass(tc,AST))):
-                raise Exception("Arguments passes to For condition must be of 'AST' type")
             retType = typecheck(block, scopes)
             
             scopes.endScope()
             return retType
-
+        
+        case Set(lineNumber, var, name, value):
+            # Type check the value
+            tv = typecheck(value, scopes)
+            # Type check the variable and get the instanceType of the object
+            var = typecheck(var, scopes)
+            # Cheking if the returned value is an instanceType
+            if (isinstance(var, type) or (not isinstance(var, instanceType))):
+                typeCheckError(f"Expected an instance", lineNumber)
+            for stmt in list(var.name.methods.values()):
+                if (isinstance(stmt, Declare)):
+                    if (stmt.var.name == name):
+                        if (stmt.dtype != tv):
+                            typeCheckError(f"Cannot set field of type {stmt.dtype} to a {tv}", lineNumber)
+            return tv
+        
         case Seq(lines):
-            ret = nil
+            ret = []
             for line in lines:
-                ret = typecheck(line, scopes)
+                retVal = typecheck(line, scopes)
+                
+                if (not isinstance(retVal, type) and isinstance(retVal, list)):
+                    for r in retVal:
+                        if ((not isinstance(r, type)) and isinstance(r, Return)):
+                            ret.append(r)
+                
+                else:
+                    if ((not isinstance(retVal, type)) and isinstance(retVal, Return)):
+                        ret.append(retVal)
+            if ret == []:
+                ret.append(nil)
             return ret
         
         case PRINT(lineNumber, exps, sep,end):
@@ -275,8 +326,6 @@ def typecheck(program: AST, scopes = None):
             end = typecheck(end, scopes)
             for exp in exps:
                 exp = typecheck(exp, scopes)
-                if (not issubclass(exp, AST)):
-                    raise Exception(f"Invalid expression {exp} in PRINT")
                 if (not issubclass(sep, Str)):
                     typeCheckError(f"Invalid separator {sep} in PRINT at {lineNumber}")
                 if (not issubclass(end, Str)):
@@ -336,9 +385,9 @@ def typecheck(program: AST, scopes = None):
                 typeCheckError(f"Cannot append an element of type {element} to a list of type {lType}.", lineNumber)
             return nil
          
-        case DeclareFun(lineNumber, Variable(_, name, id) as f, return_type, params_type, params, body):
+        case DeclareFun(lineNumber, f, return_type, params_type, params, body, function_type):
             # Declaring the function in the current scope
-            scopes.declareFun(f, FnObject(params_type, params, body, return_type)) 
+            scopes.declareFun(f, FnObject(function_type, params_type, params, body, return_type)) 
 
             # Strategy to type check the body
 
@@ -350,25 +399,47 @@ def typecheck(program: AST, scopes = None):
                 scopes.declareVariable(param, createDummyObject(param_type), param_type, False)
 
             # 3. type check the body
-            # retType = typecheck(body, scopes)
-            typecheck(body, scopes)
-
+            retTypes = typecheck(body, scopes)
             # 4. make sure that the return types are the same
-            # if (not(issubclass(retType, return_type) and issubclass(retType, return_type))):
-            #     typeCheckError(f"Not matching the expected return type of the function", lineNumber)
+            for retType in retTypes:
+                if retType != nil:
+                    retType = retType.value
+                if ((not issubclass(retType, nil)) and (not(issubclass(retType, return_type)))):
+                    typeCheckError(f"Not matching the expected return type of the function", lineNumber)
             
             # 5. return nil as declaration statements have no return type
             return nil
         
-        case FunCall(lineNumber, Variable(_, name, id) as f, args): 
-            # Get the function object
-            fn = scopes.getVariable(f)
+        case FunCall(lineNumber, f, args): 
+            # Get the method from the specified object object
+            if isinstance(f, Get):
+                fn = typecheck(f, scopes) # Would return an ClassObject 
+            else:
+                fn = scopes.getVariable(f) # Would return a FnObject 
             
+            # In case it is a class Object, we instantiate it
+            if isinstance(fn, ClassObject):
+                # Check if the type of arguments passed is correct
+                if "init" in fn.methods:
+                    params_type = fn.methods["init"].params_type
+                    if len(args) != len(params_type):
+                        raise typeCheckError(f"Number of arguments for init method of {fn.name} does not match", lineNumber, "integrityError")
+                    for i in range(len(params_type)):
+                        arg = typecheck(args[i], scopes)
+                        if (((not isinstance(params_type[i], type)) and params_type[i] != arg) or not(issubclass(arg, params_type[i]))):
+                            raise typeCheckError(f"{i+1}th Argument passed to the init function is of invalid type", lineNumber)
+
+                # In case the value arguments are passed but the init method does not exist    
+                elif len(args) > 0:
+                    raise typeCheckError(f"Class {fn.name} does not have an init method", lineNumber, "integrityError")
+                # Return the instanceType of the class
+                return instanceType(fn)
+
+            # In case of Functions
             # Generate the type of arguments
             argv = []
             for arg in args:
                 argv.append(typecheck(arg, scopes))
-            
             # Only typechecking the param type and the argument types
             p_types = fn.params_types
             for i in range(len(p_types)):
@@ -380,11 +451,77 @@ def typecheck(program: AST, scopes = None):
         
         case Return(lineNumber, exp):
             exp = typecheck(exp, scopes)
-            return exp
+            return Return(lineNumber, exp)
         
         case Block(blockStatements):
             # Return the type of the last statement in the seq in block
-            return typecheck(blockStatements, scopes)
+            retType = typecheck(blockStatements, scopes)
+            return retType
+        
+        case DeclareClass(lineNumber, var, stmts, thisID) as d:
+            # Declaring the class object
+            classObj = ClassObject(var.name, stmts, thisID)
+            scopes.declareVariable(var, classObj, ClassObject, False)
+            
+            # Performing the typeChecking for Class methods and variable declarations
+            # Begining scopes for the this variable
+            scopes.beginScope()
+            
+            # Declaring a dummy this variable
+            scopes.declareVariable(Variable(lineNumber, "this", thisID), InstanceObject(classObj, {}), instanceType(classObj), False)
+
+            for stmt in stmts:
+                stmt = stmts[stmt]
+                # Declaring the instance fields
+                if isinstance(stmt, Declare):
+                    value = typecheck(stmt.value, scopes)
+                    if (isinstance(value, type)):
+                        value = createDummyObject(value)
+                    if ((not isinstance(stmt.dtype, type)) and isinstance(stmt.dtype, instanceType)):
+                        stmt.dtype = instanceType(scopes.getVariable(stmt.dtype.name))
+                    scopes.getVariable(Variable(lineNumber, "this", thisID)).fields[stmt.var.name] = [value , stmt.dtype, stmt.isConst]
+                elif isinstance(stmt, DeclareFun):
+                    typecheck(stmt, scopes)
+                else:
+                    typeCheckError(f"Invalid statement in class definition", lineNumber)
+
+            scopes.endScope()
+            
+            return nil
+        
+        case Get(lineNumber, var, name):
+            obj =  typecheck(var, scopes)
+            if isinstance(obj, type) or not isinstance(obj, instanceType):
+                raise typeCheckError(f"Cannot access field {name} on {obj}", lineNumber, "AttributeError")
+            classObj = obj.name
+            if name not in classObj.methods:
+                raise typeCheckError(f"Cannot access field {name} on {obj}", lineNumber, "AttributeError")
+            
+            field = nil()
+            # Getting the field type
+            for attr in classObj.methods:
+                if (attr == name):
+                    field = classObj.methods[attr]
+                    if (isinstance(field, Declare)):
+                        if isinstance(field.dtype, type):
+                            return field.dtype
+                        elif isinstance(field.dtype, instanceType):
+                            return instanceType(field.dtype.name)
+                    elif (isinstance(field, DeclareFun)):
+                        # Begining scopes for the this variable
+                        scopes.beginScope()
+
+                        # Declaring the this variable
+                        scopes.declareVariable(Variable(lineNumber, "this", classObj.thisID), nil(), instanceType(classObj), False)
+
+                        # Declaring the function
+                        scopes.declareFun(field.var, FnObject(field.functionType, field.params_type, field.params, field.body, field.return_type))
+
+                        # Obtaining the function object
+                        fnObj = scopes.getVariable(field.var)
+
+                        # Returning the function object
+                        return fnObj
         
         case _ as v:
             raise Exception(f"Got {v}, Invalid Expression|Statement")
